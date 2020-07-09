a899e3f0cfc342c0acac13448ab6ce2aaf4d6b66
Yuriy Weiss <gweiss@yandex.ru>
2020-07-09 11:55:16 +0300

    #1 added table text_objects

--- db-scripts/mysql/03 create tables.sql
+++ db-scripts/mysql/03 create tables.sql
@@ -5,3 +5,11 @@ CREATE  TABLE `fractalmap`.`square` (
   `iterations` INT NOT NULL ,
   PRIMARY KEY (`layer_index`, `left_re`, `top_im`) )
 ENGINE = InnoDB;
+
+CREATE  TABLE `fractalmap`.`text_object` (
+  `re` DOUBLE NOT NULL ,
+  `im` DOUBLE NOT NULL ,
+  `min_layer_index` INT NOT NULL ,
+  `text` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`re`, `im`) )
+ENGINE = InnoDB;
