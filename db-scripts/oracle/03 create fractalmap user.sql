eabc4e09641fa964c089886a3ccc0cab10457b12
Yuriy Weiss <gweiss@yandex.ru>
2020-07-10 17:45:05 +0300

    #3 table text_object filled with data

--- db-scripts/oracle/03 create fractalmap user.sql
+++
@@ -1,5 +0,0 @@
-create user FRACTALMAP identified by fractalmap default tablespace FRCTMPD temporary tablespace TEMP;
-
-grant connect, resource to FRACTALMAP;
-
-grant select any table to FRACTALMAP;
